71259d9c3f31c3caadb203a4d759f1d42d2cbed8
Luiz Amaral <luiz.amaral@innogames.com>
2024-02-01 15:23:28 +0100

    Cleanup hypervisor attribute when retiring a VM
    We extended the libvirt_hosts check to enfore retired VMs are not
    assigned to a hypervisor, so we most ensure the attribute gets cleaned
    when a VM gets retired by igvm.

--- igvm/commands.py
+++ igvm/commands.py
@@ -694,6 +694,7 @@ def vm_delete(vm_hostname, retire=False):
         # or update its state to 'retired' if retire is True.
         if retire:
             vm.dataset_obj['state'] = 'retired'
+            vm.dataset_obj['hypervisor'] = None
             vm.dataset_obj.commit()
             log.info(
                 '"{}" is destroyed and set to "retired" state.'.format(
